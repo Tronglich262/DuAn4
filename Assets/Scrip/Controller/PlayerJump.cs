1fd92e61439b9628b50e33fdd3b5b55f328f99c1
Tronglich262 <lichtrong262@gmail.com>
2025-04-14 16:02:39 +0700

    Game

---
+++ Assets/Scrip/Controller/PlayerJump.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float jumpForce = 10f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public Transform groundCheck; // empty GameObject đặt dưới chân
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Kiểm tra chạm đất
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        animator.SetFloat("VerticalSpeed", rb.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            animator.SetTrigger("Jump");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+        else if (rb.linearVelocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.fixedDeltaTime;
+        }
+    }
+}
